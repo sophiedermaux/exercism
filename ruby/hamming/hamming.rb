bad6e2fdfd9ec8e57b301bf584c4321dcf5f6f5e
Sophie Dermaux <sophiebcdermaux@gmail.com>
2018-04-30 17:53:44 +0100

    checking index vs letters to find out order. currently arrays

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,7 @@
-class Hamming
-  def self.compute(str1, str2)
-    0
+  def hamming(str1, str2)
+    p string1 = str1.downcase.split('')
+    p string2 = str2.downcase.split('')
+    str1.each_index { |idx| puts string1.values_at(idx) }
   end
-end
+  hamming('GAGCCTACTAACGGGAT','CATCGTAATGACGGCCT')
+  # find out difference of 7
